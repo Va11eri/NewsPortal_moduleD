85e9f8cc940e52292129fe5727b344763119d35a
Va11eri <122464912+Va11eri@users.noreply.github.com>
2023-04-24 21:13:23 +0300

    filter

---
+++ News/temlatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+ERROR_WORDS = [
+    'pig',
+    'silly',
+    'foolish',
+]
+
+
+@register.filter()
+def censor(word):
+    if isinstance(word, str):
+        for i in word.split():
+            if i.capitalize() in ERROR_WORDS:
+                word = word.replace(i, i[0] + '*' * len(i))
+    else:
+        raise ValueError('Wrong format')
+    return word
+
