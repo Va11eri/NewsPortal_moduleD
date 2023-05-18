076c48c9564f6a1a533f173490903b20052ca216
Va11eri <122464912+Va11eri@users.noreply.github.com>
2023-05-18 19:14:11 +0300

    D9

--- News/signals.py
+++ News/signals.py
@@ -5,7 +5,9 @@ from django.template.loader import render_to_string
 from .models import PostCategory
 from django.conf import settings
 from django.core.mail import send_mail
-import logging
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+
 
 def send_notifications(preview, pk, title, subscribers):
     html_content = render_to_string(
@@ -41,8 +43,17 @@ def notify_about_new_post(sender, instance, **kwargs):
         send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
 
 
-def send_welcome_email(sender, request, user, **kwargs):
-    subject = 'Welcome to Newsportal! Have a wonderful day'
-    message = render_to_string('welcome_email.html', {'user': user})
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-    logging.info("send_welcome_email function called.")
+def send_welcome_email(sender, instance, created, **kwargs):
+    if created:
+        subject = 'Welcome to Newsportal! Have a wonderful day'
+        message = render_to_string('welcome_email.html', {'user': instance})
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [instance.email])
+
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def user_registered(sender, instance, created, **kwargs):
+    if created:
+        send_welcome_email(sender, instance, created, **kwargs)
